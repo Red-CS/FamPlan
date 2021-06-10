d35b68c5eff4826b3f937cf817958e15630da7d7
Red-CS <red.devcs@gmail.com>
2021-06-10 19:50:38 -0400

    Add more of landing page
    Next commit should get rid of pregenerated unused code

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,25 +3,74 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { ColorSchemeName } from "react-native";
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
+import NotFoundScreen from "../screens/NotFoundScreen";
+import { RootStackParamList } from "../types";
+import BottomTabNavigator from "./BottomTabNavigator";
+import LinkingConfiguration from "./LinkingConfiguration";
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+import { Text, View } from "../components/Themed";
+import { StyleSheet, Image } from "react-native";
+
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
+      <HomeScreen />
     </NavigationContainer>
   );
 }
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../img/login_png_1.png")} />
+      <View>
+        <Text style={styles.getStarted}>Get Started</Text>
+        <Text style={styles.logIn}>Log In</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  getStarted: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+    paddingHorizontal: 34,
+    color: "#ffffff",
+    backgroundColor: "#FF7A7A",
+  },
+  logIn: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+    paddingHorizontal: 34,
+    color: "#FF7A7A",
+  },
+});
 
 // A root stack navigator is often used for displaying modals on top of all other content
 // Read more here: https://reactnavigation.org/docs/modal
@@ -31,7 +80,11 @@ function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
     </Stack.Navigator>
   );
 }
---
+++ screens/LogInScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "../components/Themed";
+
+export default function LogInScreen() {
+  return (
+    <View>
+      <Text>Log In Screen</Text>
+    </View>
+  );
+}
